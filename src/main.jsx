27bd16507460aaa0b6dc2b20108c47e915b748c3
CamilinGarces <garces.wilson@correounivalle.edu.co>
2025-04-28 17:17:50 -0500

    Resolviendo conflictos

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Home from "./pages/home/Home";
 import Quiz from "./pages/quiz/Quiz";
-<<<<<<< HEAD
-=======
-import NotFound from "./pages/not-found/NotFound";
-import Heart from "./pages/heart/Heart";
-import HighBloodPressure from "./pages/heart/high-blood-pressure/HighBloodPressure";
-import LowBloodPressure from "./pages/heart/low-blood-pressure/LowBloodPressure";
-import InsuficienciaCardiaca from "./pages/heart/insuficiencia-cardiaca/InsuficienciaCardiaca";
-import "./index.css";
->>>>>>> origin/rem
 import Layout from "./layout/Layout";
 import Disease from "./pages/disease/Disease.jsx";
 import Us from "./pages/sobreNosotros/Us.jsx"
@@ -21,21 +12,10 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Layout>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Home />} />
         <Route path="/enfermedades/:nombre" element={<Disease />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/nosotros" element={<Us />} />
-=======
-        <Route index path="/" element={<Home />} />
-        <Route path="quiz" element={<Quiz />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="corazon" element={<Heart />}>
-          <Route path="presion-alta" element={<HighBloodPressure />} />
-          <Route path="presion-baja" element={<LowBloodPressure />} />
-          <Route path="insuficiencia-cardiaca" element={<InsuficienciaCardiaca />} />
-        </Route>
->>>>>>> origin/rem
       </Routes>
     </Layout>
   </BrowserRouter>
