91c71dabf1d78dd52f7c1d0282e64a4085e58148
SergioS022 <sergioandree122@gmail.com>
2025-04-28 20:31:45 -0500

    Errores en pagina de arritmia solucionados.

--- src/models/arritmia/modelo1/Modelo1.jsx
+++ src/models/arritmia/modelo1/Modelo1.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas } from "@react-three/fiber";
-import CloggedArtery from "./models-3d/Arritmia";
+import Arritmia from "./models-3d/Arritmia";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Lights from "./lights/Lights";
 
@@ -10,7 +10,7 @@ const Modelo1 = () => {
       <PerspectiveCamera makeDefault position={[0, 0, 5]} />
       <Lights />
       <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
-      <CloggedArtery />
+      <Arritmia />
       <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
         <planeGeometry args={[5, 5]} />
         <shadowMaterial opacity={0.3} />
--- src/models/arritmia/modelo1/models-3d/Arritmia.jsx
+++ src/models/arritmia/modelo1/models-3d/Arritmia.jsx
@@ -2,18 +2,18 @@
 import { useGLTF } from '@react-three/drei'
 
 export default function Arritmia(props) {
-  const { nodes, materials } = useGLTF('/models-3d/heart-rate-monitor.glb')
+  const { nodes , materials } = useGLTF('/models-3d/human-heart.glb')
 
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
-        //geometry={nodes.heartRateMonitor.geometry} // Verifica que esta propiedad exista en el modelo
-        material={materials.heartRateMonitorMaterial} // Verifica que esta propiedad exista en el modelo
+        geometry={nodes.Heart.geometry} // Verifica que esta propiedad exista en el modelo
+        material={materials.HeartMaterial} // Verifica que esta propiedad exista en el modelo
       />
     </group>
   )
 }
 
-useGLTF.preload('/models-3d/heart-rate-monitor.glb')
+useGLTF.preload('/models-3d/human-heart.glb')
